98e18b8931faaf2b2b75bc858eadcdd0e4cc6b0b
Methaxyz <kachai1234@hotmail.com>
2022-04-04 19:27:36 +0700

    commit

--- misery/public/game.js
+++ misery/public/game.js
@@ -14,7 +14,7 @@ const currentPlayerTurn = () => `${currentPlayer}'s turn`;
 
 statusDisplay.innerHTML = currentPlayerTurn();
 
-const winningConditions = [
+const winningConaditions = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
